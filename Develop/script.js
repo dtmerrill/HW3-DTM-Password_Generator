4398ca305210c4e2af68ba629d296a7780d0e730
Dana Merrill <dtmerrilliii@gmail.com>
2021-04-23 17:50:28 -0400

    updated with var and prompt text

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+
+
+var getLength = function() {
+  var pwLength = window.prompt("Enter a password length (between 8 and 128 characters).");
+  if (!pwLength) {
+    return;
+  }
+};
+var getLower = function() {
+  var pwLower = window.prompt("Do you wish to include lowercase letters? (Y/N)");
+  if (!pwLower) {
+    return;
+  }
+};
+var getUpper = function() {
+  var pwUpper = window.prompt("Do you wish to include uppercase letters? (Y/N)");
+  if (!pwUpper) {
+    return;
+  }
+};
+var getSpecial = function() {
+  var pwSpecial = window.prompt("Do you wish to include special characters? (Y/N)");
+  if (!pwSpecial) {
+    return;
+  }
+};
 
 // Write password to the #password input
 function writePassword() {
