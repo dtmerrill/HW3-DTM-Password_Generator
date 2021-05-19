e0e50cc0ba55bc5cd74834337ba1cf00ad490718
Dana Merrill <dtmerrilliii@gmail.com>
2021-05-18 20:09:15 -0400

    Updated to add char arrays and initial prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,31 @@
 // Assignment Code
 
+//array of characters
+var specialChar = ["!","#","$",","%"","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
+var numberChar = ["1","2","3","4","5","6","7","8","9","0"]
+var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+function getPasswordOptions() {
+  var getLength = parseInt(prompt("Enter a password length (between 8 and 128 characters)."))
+  if (Number.isNaN(getLength)) {
+    alert ("Password length must be provided as a number.")
+    return null
+  }
+  if (getLength < 8) {
+    alert ("Password must be between 8 and 128 characters.")
+    return null
+  }
+  if (getLength > 128) {
+    alert ("Password must be between 8 and 128 characters.")
+    return null
+  }
+  var hasSpecialChar = confirm("Would you like to use special characters? (Click Okay)")
+  var hasNumberChar = confirm("Would you like to use numbers? (Click Okay)")
+  var hasLowerChar = confirm("Would you like to use lowercase characters? (Click Okay)")
+  var hasUpperChar = confirm("Would you like to use uppercase characters? (Click Okay)")
+}
+
 var generateBtn = document.querySelector("#generate");
 
 
